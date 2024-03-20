e72181e3fa8707bb1d470513bea1dbaa3c586258
FelixSmlz <schmoelz.felix@gmail.com>
2024-03-20 21:44:34 +0100

    script - added code for displaying Com and Player input

---
+++ RockPapeScissors/scripts/script.js
@@ -0,0 +1,39 @@
+const inputRock = document.getElementById("rock");
+const inputPaper = document.getElementById("paper");
+const inputScissors = document.getElementById("scissors");
+
+const playerInputs = [inputRock, inputPaper, inputScissors];
+const choices = ["rock", "paper", "scissors"];
+const imagesPath = {
+  rock: "./assets/img/Rock.png",
+  paper: "./assets/img/Paper.png",
+  scissors: "./assets/img/Scissors.png",
+};
+
+let comChoice = getComChoice();
+
+function getComChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getPlayerChoice() {
+  playerInputs.forEach((input) => {
+    input.addEventListener("click", () => {
+      const playerChoice = input.id;
+      displayPlayerChoice(playerChoice);
+      comChoice = getComChoice();
+      displayChoice("choice-com-img", comChoice);
+    });
+  });
+}
+
+function displayChoice(elementId, choice) {
+  const imgElement = document.getElementById(elementId);
+  imgElement.src = imagesPath[choice];
+}
+
+function displayPlayerChoice(playerChoice) {
+  displayChoice("choice-player-img", playerChoice);
+}
+
+getPlayerChoice();
